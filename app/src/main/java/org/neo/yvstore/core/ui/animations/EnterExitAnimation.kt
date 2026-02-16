76b59127bb37d4dea7767d6d23ea3a5924ee8ecd
Nweke Onyekachukwu <55864433+Neo-glitch@users.noreply.github.com>
2026-02-16 06:10:06 +0100

    removed unused animations

--- app/src/main/java/org/neo/yvstore/core/ui/animations/EnterExitAnimation.kt
+++ app/src/main/java/org/neo/yvstore/core/ui/animations/EnterExitAnimation.kt
@@ -1,22 +1,10 @@
 package org.neo.yvstore.core.ui.animations
 
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.slideOutVertically
 
 object EnterExitTransitions {
-
-    const val FAST = 150
-    const val NORMAL = 300
-    const val SLOW = 500
-
-    val FadeIn = fadeIn(animationSpec = tween(300))
-    val FadeOut = fadeOut(animationSpec = tween(300))
-
     val SlideInFromRight = slideInHorizontally(
         animationSpec = tween(300),
         initialOffsetX = { it },
@@ -37,21 +25,4 @@ object EnterExitTransitions {
         targetOffsetX = { it },
     )
 
-    val slideInFromBottom = slideInVertically(
-        animationSpec = tween(300),
-        initialOffsetY = { it },
-    )
-
-    val SlideOutToBottom = slideOutVertically(
-        animationSpec = tween(300),
-        targetOffsetY = { it },
-    )
-
-    // Combined transitions
-    val FadeSlideInFromRight = SlideInFromRight + FadeIn
-    val FadeSlideOutToLeft = SlideOutToLeft + FadeOut
-    val FadeSlideInFromLeft = SlideInFromLeft + FadeIn
-    val FadeSlideOutToRight = SlideOutToRight + FadeOut
-    val FadeSlideInFromBottom = slideInFromBottom + FadeIn
-    val FadeSlideOutToBottom = SlideOutToBottom + FadeOut
 }
